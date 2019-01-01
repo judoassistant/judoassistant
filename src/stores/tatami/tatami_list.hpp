cd12c3001e281e3727a0dfadc75915315b08f066
Svend Christian Svendsen <svendcsvendsen@gmail.com>
2019-01-01 21:57:23 +0100

    Reimplement TatamiList

--- src/stores/tatami/tatami_list.hpp
+++ src/stores/tatami/tatami_list.hpp
@@ -16,22 +16,24 @@ class TatamiList {
 public:
     void eraseTatami(PositionHandle handle);
     bool containsTatami(PositionHandle handle) const;
+    bool containsTatami(TatamiLocation location) const;
     PositionHandle getHandle(size_t index) const;
     size_t getIndex(PositionHandle handle) const;
-    size_t getIndex(TatamiLocation handle) const;
+    size_t getIndex(TatamiLocation location) const;
     size_t tatamiCount() const;
-    void addTatami(size_t index);
-    BlockLocation refreshLocation(BlockLocation location) const;
+    TatamiLocation addTatami(size_t index);
+    BlockLocation refreshLocation(BlockLocation location, std::pair<CategoryId, MatchType> block) const;
 
     TatamiStore & operator[](PositionHandle handle);
 
-    TatamiStore & at(PositionHandle handle) const;
-    TatamiStore & at(size_t index) const;
-    TatamiStore & at(TatamiLocation location) const;
-    ConcurrentBlockGroup & at(ConcurrentGroupLocation location) const;
-    ConcurrentBlockGroup & at(SequentialGroupLocation location) const;
+    const TatamiStore & at(PositionHandle handle) const;
+    TatamiStore & at(PositionHandle handle);
+    const TatamiStore & at(size_t index) const;
+    TatamiStore & at(TatamiLocation location);
+    const TatamiStore & at(TatamiLocation location) const;
+    const ConcurrentBlockGroup & at(ConcurrentGroupLocation location) const;
+    const SequentialBlockGroup & at(SequentialGroupLocation location) const;
 
-    bool containsTatami(TatamiLocation location) const;
     void moveBlock(const TournamentStore &tournament, std::pair<CategoryId, MatchType> block, std::optional<BlockLocation> from, std::optional<BlockLocation> to);
     void recomputeBlock(const TournamentStore &tournament, BlockLocation);
 
