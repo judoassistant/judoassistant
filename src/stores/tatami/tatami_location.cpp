9166d433994aeea7a99dcd444e64a8922293eebb
Svend Christian Svendsen <svendcsvendsen@gmail.com>
2019-01-01 19:39:28 +0100

    Add tatami location classes

---
+++ src/stores/tatami/tatami_location.cpp
@@ -0,0 +1,21 @@
+#include "stores/tatami/tatami_location.hpp"
+
+// TODO: Move into stores/tatami/location.hpp
+// TODO: Consider making compares and hashes ignore index
+
+std::ostream &operator<<(std::ostream &out, const NewTatamiLocation &location) {
+    return out << location.handle;
+}
+
+std::ostream &operator<<(std::ostream &out, const ConcurrentGroupLocation &location) {
+    return out << "(" << location.handle << "; " << location.tatami << ")";
+}
+
+std::ostream &operator<<(std::ostream &out, const SequentialGroupLocation &location) {
+    return out << "(" << location.handle << "; " << location.concurrentGroup.handle << "; " << location.concurrentGroup.tatami << ")";
+}
+
+std::ostream &operator<<(std::ostream &out, const BlockLocation &location) {
+    return out << "(" << location.pos << "; " << location.sequentialGroup.handle << "; " << location.sequentialGroup.concurrentGroup.handle << "; " << location.sequentialGroup.concurrentGroup.tatami << ")";
+}
+
