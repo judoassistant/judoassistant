aeb83d75055c88d23435c328e60b64af2ba91c30
Svend Christian Svendsen <svendcsvendsen@gmail.com>
2019-01-12 19:07:12 +0100

    Add mockup web configure form

--- src/widgets/tournament_widget.cpp
+++ src/widgets/tournament_widget.cpp
@@ -1,6 +1,9 @@
-#include <QFormLayout>
+#include <QGridLayout>
 #include <QGroupBox>
+#include <QLabel>
+#include <QPushButton>
 #include <QString>
+#include <QVBoxLayout>
 
 #include "widgets/tournament_widget.hpp"
 #include "actions/tournament_actions.hpp"
@@ -8,11 +11,15 @@
 #include "stores/qtournament_store.hpp"
 #include "store_managers/store_manager.hpp"
 
+constexpr size_t FIRST_COLUMN_WIDTH = 200;
+
 TournamentWidget::TournamentWidget(StoreManager &storeManager)
     : mStoreManager(storeManager)
 {
     QVBoxLayout *mainLayout = new QVBoxLayout;
     mainLayout->addWidget(basicInformationSection());
+    mainLayout->addWidget(webSection());
+    mainLayout->setAlignment(Qt::AlignTop);
 
     setLayout(mainLayout);
 
@@ -25,24 +32,73 @@ TournamentWidget::TournamentWidget(StoreManager &storeManager)
 
 QWidget* TournamentWidget::basicInformationSection() {
     QGroupBox *box = new QGroupBox(tr("Basic tournament information"));
-    QFormLayout *formLayout = new QFormLayout;
-    box->setLayout(formLayout);
+    QGridLayout *layout = new QGridLayout(box);
 
     mNameContent = new QLineEdit;
     mNameContent->setText(QString::fromStdString(mStoreManager.getTournament().getName()));
     connect(mNameContent, &QLineEdit::editingFinished, this, &TournamentWidget::updateTournamentName);
-    formLayout->addRow(tr("Tournament name"), mNameContent);
+    layout->addWidget(new QLabel(tr("Tournament name")), 0, 0);
+    layout->addWidget(mNameContent, 0, 1);
 
     mTatamiCountContent = new QSpinBox;
     mTatamiCountContent->setMinimum(1);
     mTatamiCountContent->setValue(mStoreManager.getTournament().getTatamis().tatamiCount());
     connect(mTatamiCountContent, QOverload<int>::of(&QSpinBox::valueChanged), this, &TournamentWidget::updateTatamiCount);
-    formLayout->addRow(tr("Tatami Count"), mTatamiCountContent);
+    layout->addWidget(new QLabel(tr("Tatami Count")), 1, 0);
+    layout->addWidget(mTatamiCountContent, 1, 1);
 
     mLanguageContent = new QComboBox;
     mLanguageContent->addItem("English");
     // connect(mNameContent, &QLineEdit::editingFinished, this, &TournamentWidget::updateTournamentName);
-    formLayout->addRow(tr("Tournament language"), mLanguageContent);
+    layout->addWidget(new QLabel(tr("Tournament Language")), 2, 0);
+    layout->addWidget(mLanguageContent, 2, 1);
+
+    return box;
+}
+
+QWidget* TournamentWidget::webSection() {
+    QGroupBox *box = new QGroupBox(tr("Live Web Results"));
+    QGridLayout *layout = new QGridLayout(box);
+
+    auto mUrlLabel = new QLabel;
+    mUrlLabel->setText("Address");
+    layout->addWidget(mUrlLabel, 0, 0);
+
+    auto mUrlContent = new QLineEdit;
+    mUrlContent->setText("live.judoassistant.com/bjergkobing");
+    mUrlContent->setEnabled(false);
+    layout->addWidget(mUrlContent, 0, 1, 1, 2);
+
+    auto mStatusLabel = new QLabel;
+    mStatusLabel->setText("Status");
+    layout->addWidget(mStatusLabel, 1, 0);
+
+    auto mStatusContent = new QLineEdit;
+    mStatusContent->setText("not configured");
+    mStatusContent->setEnabled(false);
+    layout->addWidget(mStatusContent, 1, 1, 1, 2);
+
+    auto mSetupButton = new QPushButton("Configure..");
+    layout->addWidget(mSetupButton, 2, 1);
+
+    auto mNameButton = new QPushButton("Change name..");
+    mNameButton->setEnabled(false);
+    layout->addWidget(mNameButton, 2, 2);
+    // mNameContent = new QLineEdit;
+    // mNameContent->setText(QString::fromStdString(mStoreManager.getTournament().getName()));
+    // // connect(mNameContent, &QLineEdit::editingFinished, this, &TournamentWidget::updateTournamentName);
+    // formLayout->addRow(tr("Status"), mNameContent);
+
+    // mNameContent = new QLabel;
+    // mTatamiCountContent->setMinimum(1);
+    // // mTatamiCountContent->setValue(mStoreManager.getTournament().getTatamis().tatamiCount());
+    // // connect(mTatamiCountContent, QOverload<int>::of(&QSpinBox::valueChanged), this, &TournamentWidget::updateTatamiCount);
+    // formLayout->addRow(tr(""), mTatamiCountContent);
+
+    // mLanguageContent = new QComboBox;
+    // mLanguageContent->addItem("English");
+    // // connect(mNameContent, &QLineEdit::editingFinished, this, &TournamentWidget::updateTournamentName);
+    // formLayout->addRow(tr("Tournament language"), mLanguageContent);
 
     return box;
 }
--- src/widgets/tournament_widget.hpp
+++ src/widgets/tournament_widget.hpp
@@ -18,6 +18,7 @@ public:
 
 private:
     QWidget* basicInformationSection();
+    QWidget* webSection();
 
     void tournamentAboutToBeReset();
     void tournamentReset();
