2fb59713f896fb455ca5be8f9deedcf3a0fbd555
Svend Christian Svendsen <svendcsvendsen@gmail.com>
2019-04-05 16:35:13 +0200

    Enable dark fusion theme

--- src/ui/applications/hub_application.cpp
+++ src/ui/applications/hub_application.cpp
@@ -19,6 +19,23 @@ HubApplication::HubApplication(int &argc, char *argv[]) : QApplication(argc, arg
     parser.process(*this);
 
     mArgs = parser.positionalArguments();
+
+    setStyle(QStyleFactory::create("fusion"));
+
+    QPalette palette;
+    palette.setColor(QPalette::AlternateBase, QColor("#9bdb4d"));
+    palette.setColor(QPalette::Base, QColor("#3d4248")); // Background of text areas
+    palette.setColor(QPalette::BrightText, Qt::red);
+    palette.setColor(QPalette::Button, QColor("#3d4248"));
+    palette.setColor(QPalette::ButtonText, Qt::white);
+    palette.setColor(QPalette::Highlight, QColor("#3689e6"));
+    palette.setColor(QPalette::HighlightedText, Qt::white);
+    palette.setColor(QPalette::Text, Qt::white);
+    // palette.setColor(QPalette::ToolTipBase, Qt::white);
+    // palette.setColor(QPalette::ToolTipText, Qt::white);
+    palette.setColor(QPalette::Window, QColor("#36393e")); // Background of border area
+    palette.setColor(QPalette::WindowText, Qt::white);
+    setPalette(palette);
 }
 
 int HubApplication::exec() {
