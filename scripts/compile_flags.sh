3887b0931059bf248a9dafe67eac46481223f8aa
Svend Christian Svendsen <svendcsvendsen@gmail.com>
2019-04-09 20:48:42 +0200

    Add flags build script

---
+++ scripts/compile_flags.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SRC_DIR="$MESON_SOURCE_ROOT/flags/flags/4x3/"
+OUTPUT_DIR="$MESON_BUILD_ROOT/flags"
+
+# INPUT_COLOR="#000000"
+# OUTPUT_COLOR="#000000"
+
+rm -rf $OUTPUT_DIR
+mkdir -p $OUTPUT_DIR
+
+function generate_flag {
+    # sed -e "s/$INPUT_COLOR/$OUTPUT_LIGHT_COLOR/" "$SRC_DIR/$1" > "$LIGHT_DIR/$2"
+    cp "$SRC_DIR/$1.svg" "$OUTPUT_DIR/$1.svg";
+}
+
+generate_flag "dk"
+generate_flag "gb"
+generate_flag "fr"
