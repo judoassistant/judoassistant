d909f9331ee0f1a6c8986582c88fb4353b84d558
Svend Christian Svendsen <svendcsvendsen@gmail.com>
2019-08-19 20:48:05 +0200

    Update Windows packaging scripts with DLL paths

--- package_windows.py
+++ package_windows.py
@@ -13,5 +13,14 @@ subprocess.check_call(['ninja install'], cwd=builddir)
 # Copy extra files
 shutil.copy('judoassistant.iss', pkgdir, cwd=builddir)
 
+# Copy DLLs
+shutil.copy(r'C:\Libraries\boost_1_70_0\lib64-msvc-14.2\boost_filesystem-vc142-mt-x64-1_70.dll', pkgdir)
+shutil.copy(r'C:\Libraries\OpenSSL-Win64\bin\libcrypto-1_1-x64.dll', pkgdir)
+shutil.copy(r'C:\Libraries\OpenSSL-Win64\bin\libssl-1_1-x64.dll', pkgdir)
+shutil.copy(r'C:\Libraries\Qt\5.13.0\msvc2017_64\bin\Qt5Core.dll', pkgdir)
+shutil.copy(r'C:\Libraries\Qt\5.13.0\msvc2017_64\bin\Qt5Gui.dll', pkgdir)
+shutil.copy(r'C:\Libraries\Qt\5.13.0\msvc2017_64\bin\Qt5Widgets.dll', pkgdir)
+shutil.copy(r'C:\Libraries\zstd-v1.4.2-win64\dll\libzstd.dll', pkgdir)
+
 # Run Inno Setup 6
 subprocess.check_call([r'C:\Program Files (x86)\Inno Setup 6\ISCC.exe', 'judoassistant.iss'], cwd=pkgdir)
