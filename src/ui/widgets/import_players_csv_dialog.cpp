205bf2e9102ce6325adeb2d12d31d183f21b70bc
Svend Christian Svendsen <svendcs@svendcs.com>
2020-03-11 20:52:51 +0100

    Fix country column in csv dialog

--- src/ui/widgets/import_players_csv_dialog.cpp
+++ src/ui/widgets/import_players_csv_dialog.cpp
@@ -227,10 +227,10 @@ void ImportPlayersCSVDialog::setWeightColumn(int index) {
 }
 
 void ImportPlayersCSVDialog::setCountryColumn(int index) {
-    auto oldValue = mImporter.getSexColumn();
+    auto oldValue = mImporter.getCountryColumn();
     auto newValue = (index == 0 ? std::nullopt : std::make_optional(static_cast<size_t>(index - 1)));
 
-    mImporter.setSexColumn(newValue);
+    mImporter.setCountryColumn(newValue);
 
     if (oldValue)
         updatePreviewColumn(*oldValue);
