62cd160a491525e53baefa12668a6c3405edb687
Svend Christian Svendsen <svendcs@svendcs.com>
2020-10-22 16:41:29 +0200

    Package Qt platform dll on Windows

--- ci/package-windows.bat
+++ ci/package-windows.bat
@@ -3,6 +3,7 @@ cd build-release
 ninja install || Exit 1
 
 REM Copy libraries
+mkdir pkg\platforms
 copy C:\Libraries\boost_1_70_0\lib64-msvc-14.2\boost_filesystem-vc142-mt-x64-1_70.dll pkg
 copy C:\Libraries\OpenSSL-Win64\bin\libcrypto-1_1-x64.dll pkg
 copy C:\Libraries\OpenSSL-Win64\bin\libssl-1_1-x64.dll pkg
@@ -10,6 +11,7 @@ copy C:\Libraries\zstd-v1.4.2-win64\dll\libzstd.dll pkg
 copy C:\Libraries\Qt\5.13.0\msvc2017_64\bin\Qt5Core.dll pkg
 copy C:\Libraries\Qt\5.13.0\msvc2017_64\bin\Qt5Gui.dll pkg
 copy C:\Libraries\Qt\5.13.0\msvc2017_64\bin\Qt5Widgets.dll pkg
+copy C:\Libraries\Qt\5.13.0\msvc2017_64\plugins\platforms\qwindows.dll pkg\platforms
 copy C:\Windows\Fonts\NotoSans-Bold.ttf pkg
 copy C:\Windows\Fonts\NotoSans-Regular.ttf pkg
 copy C:\Windows\Fonts\NotoSansMono-Bold.ttf pkg
